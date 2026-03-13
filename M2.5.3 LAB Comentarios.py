c4c9966b1ccff60d6c6218be74bb16a36285d06b
Flores-Byron <gonzalezbyron256@gmail.com>
2026-03-13 12:13:20 -0600

    Create M2.5.3 LAB Comentarios.py

---
+++ M2.5.3 LAB Comentarios.py
@@ -0,0 +1,10 @@
+#este programa calcula los segundos en cierto número de horas determinadas
+
+horas = 2 
+segundos = 3600 
+
+print("Horas: ", horas) #imprime el numero de horas
+print("Segundos en Horas: ", horas * segundos) # se imprime el numero de segundos en determinado numero de horas
+print("adios")
+
+#este el es fin del programa que calcula el numero de segundos en 2 horas
