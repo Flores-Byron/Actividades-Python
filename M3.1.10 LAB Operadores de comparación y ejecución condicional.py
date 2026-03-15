ef65d0e56c5d6b270e160a0036a3151a7e0e4e7f
Flores-Byron <gonzalezbyron256@gmail.com>
2026-03-14 23:40:43 -0600

    Create M3.1.10 LAB Operadores de comparación y ejecución condicional.py

---
+++ M3.1.10 LAB Operadores de comparación y ejecución condicional.py
@@ -0,0 +1,9 @@
+entrada = input("Introduce el nombre de la flor: ")
+
+if entrada == "ESPATIFILO":
+    print("Si, ¡El ESPATIFILIO es la mejor planta de todos los tiempos!")
+elif entrada == "espatifilo":
+    print("No, ¡quiero un gran ESPATIFILO!")
+else:
+    print("¡ESPATIFILO!, ¡No", entrada + "!")
+	
