8e1695da0e66a24a5744614eeb6eafd5a73f9409
Flores-Byron <gonzalezbyron256@gmail.com>
2026-03-14 19:30:25 -0600

    Update M2.6.9 LAB Entradas y salidas simples.py

--- M2.6.9 LAB Entradas y salidas simples.py
+++ M2.6.9 LAB Entradas y salidas simples.py
@@ -1,9 +1,9 @@
 a = float(input("ingresa un valor flotante para la variable a: "))
 b = float(input("ingresa un valor flotante para la variable b: "))
 
-print("resultado de la suma " + str(a + b))
-print("resultado de la resta " + str(a - b))
-print("resultado de la multiplicacion " + str(a * b))
-print("resultado de la division " + str(a / b))
+print("resultado de la suma ", a + b)
+print("resultado de la resta ", a - b)
+print("resultado de la multiplicacion ", a * b)
+print("resultado de la division ", a / b)
 
 print("\n¡Eso es todo, amigos!")
