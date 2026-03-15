c54ad25a926d09c780969bc6f8ba0c572ff9307f
Flores-Byron <gonzalezbyron256@gmail.com>
2026-03-15 12:09:40 -0600

    Create M3.2.07 LAB Fundamentos del bucle for – contando lentamente (mississippily).py

---
+++ M3.2.07 LAB Fundamentos del bucle for – contando lentamente (mississippily).py
@@ -0,0 +1,8 @@
+import time
+tiempo = 1
+
+for tiempo in range(1, 6):# Escribe un bucle for que cuente hasta cinco.
+    print(tiempo, " Mississipi")# Cuerpo del bucle: imprime el número de iteración del bucle y la palabra "Mississippi".
+    time.sleep(1)# Cuerpo del bucle, emplea : time.sleep(1)
+
+print("Listos o no, aqui voy")# Escribe una función print con el mensaje final.
