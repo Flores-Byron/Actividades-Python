e301758a600879f59c7a7c932d592efeacef10b9
Flores-Byron <gonzalezbyron256@gmail.com>
2026-03-15 12:25:33 -0600

    Create M3.2.9 LAB La sentencia break - atrapado en un bucle.py

---
+++ M3.2.9 LAB La sentencia break - atrapado en un bucle.py
@@ -0,0 +1,7 @@
+palabra = input("dame una palabra o escribe chupacabra para salir del bucle: ")
+
+while palabra != "chupacabra":
+    if palabra == "chupacabra":
+        break
+    print("que interesante palabra")
+print("Has dejado el bucle con exito")
