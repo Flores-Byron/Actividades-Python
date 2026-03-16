dfff891ce4261079847ed5770d8cdab1567cf8a2
Flores-Byron <gonzalezbyron256@gmail.com>
2026-03-15 18:11:59 -0600

    Create M3.4.06 LAB Los fundamentos de las listas.py

---
+++ M3.4.06 LAB Los fundamentos de las listas.py
@@ -0,0 +1,11 @@
+
+hat_list = [1, 2, 3, 4, 5]  # Esta es una lista existente de números ocultos en el sombrero.
+
+num=int(input("Que numero reemplazara al número de en medio:"))# Paso 1: escribe una línea de código que solicite al usuario
+hat_list[2] = num# reemplazar el número de en medio con un número entero ingresado por el usuario.
+
+del hat_list[4]# Paso 2: escribe aquí una línea de código que elimine el último elemento de la lista.
+
+print("Longitud de la nueva lista:", len(hat_list))# Paso 3: escribe aquí una línea de código que imprima la longitud de la lista existente.
+
+print(hat_list)
